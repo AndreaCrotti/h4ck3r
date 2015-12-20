c35ba69a4fab21ec0728b7116771b8cd58f9d502
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-12-20 11:44:50 +0000

    first commit

---
+++ test/h4ck3r/handler_test.clj
@@ -0,0 +1,14 @@
+(ns h4ck3r.handler-test
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [h4ck3r.handler :refer :all]))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (mock/request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+
+  (testing "not-found route"
+    (let [response (app (mock/request :get "/invalid"))]
+      (is (= (:status response) 404)))))
