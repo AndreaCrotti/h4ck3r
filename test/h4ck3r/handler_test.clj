84622344569c62088eb3c62ad24e4697729e3826
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-12-20 15:29:52 +0000

    fix tests with some more magic

--- test/h4ck3r/handler_test.clj
+++ test/h4ck3r/handler_test.clj
@@ -5,14 +5,16 @@
             [h4ck3r.handler :refer :all]))
 
 (def desired
-  (json/write-str {:mapping {"Hello" "He", "world" "wo"}
-                   :shortened "He wo"}))
+  {"mapping" {"Hello" "He", "world" "wo"}
+   "shortened" "He wo"})
 
 (deftest test-app
   (testing "translator"
-    (let [response (app (mock/request :post "/translate" {:message "Hello world"}))]
-      (is (= 200 (:status response)))
-      (is (= desired (:body response)))))
+    (let [response (app (mock/request :post "/translate" {:message "Hello world"}))
+          data (json/read-str (:body response))]
+
+      (is (= 201 (:status response)))
+      (is (= desired data))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
